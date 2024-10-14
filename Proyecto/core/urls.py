0587d4852868693a86869a71e6bdd022a55ae98f
Diego Araya <diegoaraya641@gmail.com>
2024-10-14 20:46:58 -0300

    c

--- Proyecto/core/urls.py
+++ Proyecto/core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
-from core.views import home , inicio, registro,agregar_administrador 
+from core.views import home , registro,agregar_administrador 
 
 
 urlpatterns = [
-    path('', inicio, name="inicio"),
+    path('', home, name="home"),
     path('home', home, name="home"),
-    path('inicio', inicio, name="inicio"),
     path('registro',registro, name="registro"),
     path('agregar_administrador', agregar_administrador, name="agregar_administrador"),
     
